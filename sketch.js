7799c1c7be65cdf547d24c490a8a1109ea057f59
svitax <svitaxiom@gmail.com>
2017-09-17 19:00:56 -0700

    made alien looking thing

--- sketch.js
+++ sketch.js
@@ -1,8 +1,29 @@
 function setup() {
-  createCanvas(400,400);
-  background(55,55);
+  createCanvas(640,360);
+
 }
 
 function draw() {
-  ellipse(mouseX, mouseY, 100, 100);
+
+  background(150,150,150);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+
+  // body
+  fill(255,0,0);
+  rect(240, 145, 20, 100);
+
+  // Head
+  fill(0,0,255, 100);
+  ellipse(240, 115, 60, 60);
+
+  // Eyes
+  fill (0, 255, 0);
+  ellipse(221, 115, 16, 32);
+  ellipse(259, 115, 16, 32);
+
+  // Legs
+  line(230,195,220,205);
+  line(250,195,260,205);
+
 }

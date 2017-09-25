fbf922029c38160f466415f88c817475648cbc84
svitax <svitaxiom@gmail.com>
2017-09-24 21:51:21 -0700

    libraries

--- lesson_1/sketch.js
+++ lesson_1/sketch.js
@@ -1,29 +1,29 @@
-// function setup() {
-//   createCanvas(640,360);
-//
-// }
-//
-// function draw() {
-//
-//   background(150,150,150);
-//   ellipseMode(CENTER);
-//   rectMode(CENTER);
-//
-//   // body
-//   fill(255,0,0);
-//   rect(240, 145, 20, 100);
-//
-//   // Head
-//   fill(0,0,255, 100);
-//   ellipse(240, 115, 60, 60);
-//
-//   // Eyes
-//   fill (0, 255, 0);
-//   ellipse(221, 115, 16, 32);
-//   ellipse(259, 115, 16, 32);
-//
-//   // Legs
-//   line(230,195,220,205);
-//   line(250,195,260,205);
-//
-// }
+function setup() {
+  createCanvas(640,360);
+
+}
+
+function draw() {
+
+  background(150,150,150);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+
+  // body
+  fill(255,0,0);
+  rect(240, 145, 20, 100);
+
+  // Head
+  fill(0,0,255, 100);
+  ellipse(240, 115, 60, 60);
+
+  // Eyes
+  fill (0, 255, 0);
+  ellipse(221, 115, 16, 32);
+  ellipse(259, 115, 16, 32);
+
+  // Legs
+  line(230,195,220,205);
+  line(250,195,260,205);
+
+}
--- lesson_2/sketch.js
+++ lesson_2/sketch.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var circle = {
   x: 0,
   y: 200,
@@ -9,10 +8,7 @@ var circle = {
 var r = 218;
 var g = 160;
 var b = 221;
-=======
-var r = 0;
-var b = 225;
->>>>>>> 14c4104196a7ab69ee97c553a5c6df56c742cfe8
+
 
 function setup() {
   createCanvas(600, 400);
@@ -20,7 +16,6 @@ function setup() {
 
 function draw() {
   // background
-<<<<<<< HEAD
   background(r, g, b);
   // ellipse
   fill(250, 200, 200);
@@ -28,16 +23,3 @@ function draw() {
 
   circle.x = circle.x + 1;
 }
-=======
-  //col = mouseX;
-  //col = map(mouseX, 0, 600, 0, 255);
-  r = map(mouseX, 0, 600, 0, 255);
-  b = map(mouseX, 0, 600, 255, 0);
-  // background(col);
-  background(r, 0, b);
-
-  // ellipse
-  fill(250, 118, 222);
-  ellipse(mouseX, 200, 64, 64);
-}
->>>>>>> 14c4104196a7ab69ee97c553a5c6df56c742cfe8

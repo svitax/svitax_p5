59c35fd48aa6fa317e347349d5f54ed319e59b5d
svitax <svitaxiom@gmail.com>
2017-09-29 07:57:34 -0700

    changed let to const

--- lesson_2/sketch.js
+++ lesson_2/sketch.js
@@ -1,13 +1,13 @@
-let circle = {
+const circle = {
   x: 0,
   y: 200,
   diameter: 50,
 };
 
 
-let r = 218;
-let g = 160;
-let b = 221;
+const r = 100;
+const g = 155;
+const b = 221;
 
 
 function setup() {
@@ -21,5 +21,5 @@ function draw() {
   fill(250, 200, 200);
   ellipse(circle.x, circle.y, circle.diameter, circle.diameter);
 
-  circle.x = circle.x + 1;
+  circle.x += 1;
 }
